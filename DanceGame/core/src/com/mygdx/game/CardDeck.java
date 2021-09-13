7c99bf519e157d353ec15d2c92df1d701a64c07d
hedy-99 <hedypet@hotmail.com>
2021-09-13 14:49:46 +0200

    can now open and discard cards. Started on methods to be used when one uses a card.

--- DanceGame/core/src/com/mygdx/game/CardDeck.java
+++ DanceGame/core/src/com/mygdx/game/CardDeck.java
@@ -1,10 +1,12 @@
 package com.mygdx.game;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
 
 public class CardDeck {
+    // thinking about using maps with enums instead, but not sure
     private Enum<DanceType> danceType;
     private List<Card> deck;
     private List<Card> open;
@@ -18,6 +20,41 @@ public class CardDeck {
         // if they are predefined we should probably shuffle them before playing
         ShuffleDeck(this.deck);
 
+        this.closed = CopyCards(this.deck);
+        // Starts with two open cards
+        OpenCard();
+        OpenCard();
+
+    }
+
+    public void UseCard(int i){
+        DiscardCard(i);
+        OpenCard();
+    }
+
+    // don't need to open cards outside.
+    private void OpenCard(){
+        this.open.add(this.closed.get(0));
+        closed.remove(0);
+    }
+
+    private void DiscardCard(int i){
+        this.discarded.add(this.open.remove(i));
+    }
+
+    //might need to be public later
+    private void AddDiscCards(){
+        int l = this.discarded.size();
+        for(int i = 0; i < l; i++){
+            this.deck.add(this.discarded.remove(i));
+        }
+    }
+
+    // not sure if necessary or not
+    private List<Card> CopyCards(List<Card> deck){
+        int l = deck.size();
+
+        return new ArrayList<>(deck);
     }
 
     // Fisher-yates shuffle https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
