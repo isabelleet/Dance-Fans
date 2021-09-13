55322c96a68d783c72efec6069434c3eaddefa4c
hedy-99 <hedypet@hotmail.com>
2021-09-13 14:27:43 +0200

    can now shuffle cards if needed, used Fisher-Yates.

--- DanceGame/core/src/com/mygdx/game/CardDeck.java
+++ DanceGame/core/src/com/mygdx/game/CardDeck.java
@@ -2,6 +2,7 @@ package com.mygdx.game;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 public class CardDeck {
     private Enum<DanceType> danceType;
@@ -10,7 +11,26 @@ public class CardDeck {
     private List<Card> closed;
     private List<Card> discarded;
 
-    public CardDeck(Enum<DanceType> danceType){
+    // should decks be predefined? or have a random selection of cards of the right type?
+    public CardDeck(Enum<DanceType> danceType, List<Card> deck){
+        this.danceType = danceType;
+        this.deck = deck;
+        // if they are predefined we should probably shuffle them before playing
+        ShuffleDeck(this.deck);
+
+    }
+
+    // Fisher-yates shuffle https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
+    private void ShuffleDeck(List<Card> cards){
+        Random random = new Random();
+        int l = cards.size();
+
+        for(int i = l-1; i > 1; i--){
+            int a = random.nextInt(i+1);
+            Card temp = cards.get(a);
+            cards.set(a, cards.get(i));
+            cards.set(i, temp);
+        }
 
     }
 
