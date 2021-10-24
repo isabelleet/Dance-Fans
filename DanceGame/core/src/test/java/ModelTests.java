f63aedad82cf88de78cbc1be7287b50530ef9520
blixtcorp <jakobpersson007@gmail.com>
2021-10-24 13:58:06 +0200

    fixed a bug with addDancefanTest and removed unnecessary comments

--- DanceGame/core/src/test/java/ModelTests.java
+++ DanceGame/core/src/test/java/ModelTests.java
@@ -1,16 +1,9 @@
-import com.mygdx.game.model.Card;
-import com.mygdx.game.model.CardDeck;
 import com.mygdx.game.model.Game_Model;
 import com.mygdx.game.model.Player;
-import com.mygdx.game.model.Dancer;
-import com.mygdx.game.model.DanceFan;
 import com.mygdx.game.model.DanceFloor;
-import com.mygdx.game.model.DanceFloorTile;
 import com.mygdx.game.model.Coordinates;
 import com.mygdx.game.Enums.Type;
-import com.mygdx.game.Enums.Color;
 import com.mygdx.game.Enums.PatternOccupant;
-import org.junit.BeforeClass;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -34,23 +27,6 @@ public class ModelTests {
         model = new Game_Model();
         model.startNewGame();
     }
-
-
-    // This test isn't finished and was started before the big refactoring by Hedy
-    // If somebody is going to refactor how player turns work, might as well add this test after that
-/*
-    @Test
-    public void actuallyChangePlayer(){
-        // I guess you can start a new game in the test?
-        Player player1 = gameState.players[0];
-        Player player2 = gameState.players[1];
-        currentPlayer = gameState.players.currentPlayer().playerTurnSlot;
-        changeWhichPlayersTurnItIs();
-        newPlayer = gameState.players.currentPlayer().playerTurnSlot;
-        assertNotEquals(currentPlayer, newPlayer);
-    }
-    */
-
 
     @Test
     public void movedMainDancerOneStepRight(){
@@ -78,7 +54,6 @@ public class ModelTests {
         Coordinates stepUpAndRightOfMainDancer = new Coordinates(mainDancerCoordinates.getX() + 1, mainDancerCoordinates.getY() + 1);
 
         model.moveMainDancerOfCurrentPlayerToCoords(stepUpAndRightOfMainDancer);
-
 
         Coordinates updatedMainDancerCoordinates = model.currentPlayer().getPreviewCoordinates();
 
@@ -147,65 +122,26 @@ public class ModelTests {
         assertNotEquals(isTurnStarted, updatedIsTurnStarted );
     }
 
-    // Skrevs i onödan då allt refactorerades, antagligen ta bort?
-    /*
-    @Test
-    public void convertToMatrixTest() {
-        //check if conversion was done right by looking for index mapwidthInTiles + 1 in array, or (x = 1, y = 1)
-        int indexToCheck = model.danceFloor.mapWidthInTiles + 1;
-        model.previewDanceFloor.newDancerOnTile(indexToCheck, new DanceFan("redDanceFan"));
-        int[] coords = model.indexToCoords(indexToCheck);
-        int x = coords[0];
-        int y = coords[1];
-        DanceFloorTile[][] matrixDanceFloor = model.convertToMatrix(model.previewDanceFloor.danceFloorTiles);
-        assertEquals(matrixDanceFloor[x][y].getOccupantName(), model.previewDanceFloor.danceFloorTiles[indexToCheck].getOccupantName());
-    }
-    */
-
     @Test
     public void addADancefan() {
+        PatternOccupant E = PatternOccupant.EMPTY;
+        PatternOccupant DF = PatternOccupant.DANCEFAN;
+        PatternOccupant MD = PatternOccupant.MAINDANCER;
 
         Coordinates mainDancerCoordinates = model.currentPlayer().getPreviewCoordinates();
-        Coordinates oneStepLeftOfMainDancer = new Coordinates(mainDancerCoordinates.getX() - 1, mainDancerCoordinates.getY());
+        Coordinates threeStepsLeftOfMainDancer = new Coordinates(mainDancerCoordinates.getX() - 3, mainDancerCoordinates.getY());
 
-        //get type of occupant before we add the pattern of new dance fans to the dance floor
+        Type tileType = model.getPreviewDanceFloor().getType(threeStepsLeftOfMainDancer);
 
-        Type tileType = model.getPreviewDanceFloor().getType(oneStepLeftOfMainDancer);
-        Color tileColor = model.getPreviewDanceFloor().getColor(oneStepLeftOfMainDancer);
-
-        //a pattern to add a dance fan to the left of the main dancer
-        PatternOccupant[][] danceMovePattern = new PatternOccupant[][]{{PatternOccupant.DANCEFAN, PatternOccupant.MAINDANCER, PatternOccupant.EMPTY}};
+        PatternOccupant[][] danceMovePattern = new PatternOccupant[][]
+                {{DF, E, E, MD, E, E, E}};
 
         model.getDanceFloor().addDFromPattern(mainDancerCoordinates, model.currentPlayer().getTransparentDanceFan(), danceMovePattern);
+        model.changeWhichPlayersTurnItIs();
 
-        //Check that a dancefan was actually added to the dancefloor
-        Type updatedTileType = model.getPreviewDanceFloor().getType(oneStepLeftOfMainDancer);
-        Color updatedTileColor = model.getPreviewDanceFloor().getColor(oneStepLeftOfMainDancer);
+        Type updatedTileType = model.getDanceFloor().getType(threeStepsLeftOfMainDancer);
 
-
-        /* This became complicated since we (should at least) add previews at once when the game begins.
-          And the pattern is randomized based on which dance move cards you get. So it could sometimes work and sometimes not.
-          So for now we just made some if statements to check depending on state of cards.
-         */
-        Boolean isDancer = (tileType == Type.MD) || (tileType == Type.DF) || (tileType == Type.TRANSDF);
-        Boolean hasDancerTheCurrentPlayerColor = tileColor == model.currentPlayer().getColor();
-
-        if (tileType == Type.EMPTY) {
-            assertNotEquals(tileType, updatedTileType);
-            assertNotEquals(tileColor, updatedTileColor);
-        }
-        else if (
-                isDancer && hasDancerTheCurrentPlayerColor
-        )
-        {
-            //Then it should stay the same
-            assertEquals(tileType, updatedTileType);
-        }
-        else //isDancer but not same color
-        {
-            assertNotEquals(tileType, updatedTileType);
-        };
-
+        assertNotEquals(tileType, updatedTileType);
 
     }
 
@@ -218,7 +154,6 @@ public class ModelTests {
             if (model.getHasPlayerStartedTheirTurn()) {
                 model.playerConfirmedDanceMove();
             }
-
         }
 
         assertTrue(model.isGameDone());
@@ -269,7 +204,7 @@ public class ModelTests {
         PatternOccupant DF = PatternOccupant.DANCEFAN;
         PatternOccupant MD = PatternOccupant.MAINDANCER;
 
-        DanceFloor danceFloor = model.getDanceFloor();
+        DanceFloor danceFloor = model.getPreviewDanceFloor();
 
         PatternOccupant[][] pattern = new PatternOccupant[][]{
                 {E, DF, E},
@@ -319,14 +254,5 @@ public class ModelTests {
 
         assertEquals(model.currentPlayer().getCoordinates().getX(), coords.getX());
     }
-
-
-    //List of tests one could do:
-
-
-    // Not possible to move on top of other maindancer
-    // Not possible to go further than active card's movement distance constraint
-    // Make sure game ends if all tiles are filled (including current main dancer
-    //
 
 }
